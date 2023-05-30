5a245669a08a2a80258f7ab0d881d0e1c5838181
Barbara Souza <babiholiveira@outlook.com>
2023-05-30 14:34:18 -0300

    remove unnused files

--- src/Componentes/SideMenu/index.jsx
+++ src/Componentes/SideMenu/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaChevronLeft, FaChevronRight, FaHome, FaUser, FaCog } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaHome } from 'react-icons/fa';
 import './index.css';
 function SideMenu() {
   const [isExpanded, setIsExpanded] = useState(true);
@@ -19,30 +19,6 @@ function SideMenu() {
               </>
             ) : (
               <FaHome />
-            )}
-          </a>
-        </li>
-        <li>
-          <a href="/exemple">
-            {isExpanded ? (
-              <>
-                <FaUser />
-                <span>Exemple</span>
-              </>
-            ) : (
-              <FaUser />
-            )}
-          </a>
-        </li>
-        <li>
-          <a href="/settings">
-            {isExpanded ? (
-              <>
-                <FaCog />
-                <span>Settings</span>
-              </>
-            ) : (
-              <FaCog />
             )}
           </a>
         </li>
--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,15 +1,11 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from '../Pages/Home'
-import Settings from '../Pages/Settings'
-import Exemple from '../Pages/Exemple'
 export default function Router() {
   return (
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} exact></Route>
-      <Route path="/settings" element={<Settings />} ></Route>
-      <Route path="/exemple" element={<Exemple />} ></Route>
     </Routes>    
    </BrowserRouter>
   );
