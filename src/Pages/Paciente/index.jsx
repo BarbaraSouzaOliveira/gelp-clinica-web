80f4ef82e73c7db66c8134eccc83b242bb891a2d
Barbara Souza <babiholiveira@outlook.com>
2023-06-02 18:39:40 -0300

    fix array patient

--- src/Pages/Paciente/index.jsx
+++ src/Pages/Paciente/index.jsx
@@ -4,12 +4,30 @@ import Grid from '../../Componentes/Grid'
 
 
 export default function Paciente(){
+    const pacientes = [
+        {
+            name: 'Bruna',
+            numberOne: '',
+            numberTwo: '',
+            CPF: '',
+            email: ''
+        },
+        {
+            name: 'Barbara',
+            numberOne: '',
+            numberTwo: '',
+            CPF: '',
+            email: ''
+        },
+        ]
+
+
     return(
         <div>
             <Grid
               contentTable="Paciente"
               nameTable="Paciente"
-              itemTable={[{name:'Nome'}, {Number:'Celular1'},{Number:'Celular2'},{String:"CPF"},{String:"Email"}]}
+              itemTable={pacientes}
               />
         </div>
     );
